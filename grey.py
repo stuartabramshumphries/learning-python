57f6933602b94764f926b5257c4807f691d328dd
Stuart Abrams-Humphries <stuartabramshumphries@gmail.com>
2013-04-25 13:49:06 +0100

    sched ci

--- grey.py
+++ grey.py
@@ -64,21 +64,12 @@ def extractdata(filedogname,dogname):
 	   fd2.write(line)
 
 
-#         if '<td class="RCelement"><a href="' in line:
-#	 	print line
-#	 elif re.search('\s+\<\/table>',line):
-#	 	print "AAAARGH"
 	fd.close
 #	os.remove(filedogname)
 	fd2.close
 	analyse_data(dogname)
 
-def analyse_data(dogname):
-	''' this function analyses the dogs racing history
-	shall we graph this and the moving average?
-	certainly want to do a rating '''
-	import re
-	print "in analyse_data "
+def analyse_data(dogname): 
 	filedogname2=dogname + "-race-history.txt"
 	fd2=open(filedogname2,"r")
 	data=fd2.readlines()
@@ -88,47 +79,32 @@ def analyse_data(dogname):
 		   line=line.replace('<td class="RCelement"><a href="res_race_result.php?raceid=','')		   
 		   line=line.replace('</a></td>','')
 		   line=re.sub(r"^.*\>",'',line)
-                   print i,line
-		 elif i == 1 or (i+1) % 16 == 0:
-		   line=re.sub(r"^.*>",'',line)
-		   line2=re.sub(r'</td class="RCelement">','',line)
-		   line3=re.sub(r"</td>$",'',line2)
-		   print "dist ",i,line3
-		 elif i == 3 or (i+3) % 16 ==0:
-		   line=re.sub(r"^.*>",'',line)
-		   line2=re.sub(r'</td class="RCelement">','',line)
-		   line3=re.sub(r"</td>$",'',line2)
-		   print i,line3
-		 elif i == 4 or (i+4) % 16 == 0:
-		  line=re.sub(r"^.*>",'',line)
-		  line2=re.sub(r'</td class="RCelement">','',line)
-		  line3=re.sub(r"</td>$",'',line)
-		  print i,line3
-		 elif i== 9 or (i+9) % 16 ==0:
-		  line=re.sub(r"^.*>",'',line)
-		  line2=re.sub(r'</td class="RCelement">','',line)
-		  line3=re.sub(r"</td>$",'',line2)
-		  print i, line3
-		 elif i == 12 or (i + 12) % 16 ==0:
-		  line=re.sub(r"^.*>",'',line)
-		  line2=re.sub(r'</td class="RCelement">','',line)
-		  line3=re.sub(r"</td>$",'',line2)
-		  print i, line3
-		 elif i == 13 or (i + 13) % 16 ==0:
-		  line=re.sub(r"^.*>",'',line)
-		  line2=re.sub(r'</td class="RCelement">','',line)
-		  line3=re.sub(r"</td>$",'',line2)
-		  print i, line3
-	# what I want here is to extract the racehistory into an array
-	#	lines of interest
-	# this info repeats every 16 lines until the EOF
-	date=0
-	dis=1
-	brk=3
-	pos=4
-	tim=9
-	grade=12
-	calctim=13
+                   print line,
+		 elif i == 1 or (i-1) % 16 == 0:
+		   line=line.replace('<td class="RCelement">','')
+		   line=re.sub(r"</td>$",'',line)
+		   print line,
+		 elif i == 3 or (i-3) % 16 ==0:
+		   line=line.replace('<td class="RCelement">','')
+		   line=re.sub(r"</td>$",'',line)
+		   print line,
+		 elif i == 4 or (i-4) % 16 == 0:
+		  line=line.replace('<td class="RCelement">','')
+		  line=re.sub(r"</td>$",'',line)
+		  print line,
+		 elif i== 9 or (i-9) % 16 ==0:
+		  line=line.replace('<td class="RCelement">','')
+		  line=re.sub(r"</td>$",'',line)
+		  print line,
+		 elif i == 12 or (i - 12) % 16 ==0:
+		  line=line.replace('<td class="RCelement">','')
+		  line=re.sub(r"</td>$",'',line)
+		  print line,
+		 elif i == 13 or (i - 13) % 16 ==0:
+		  line=line.replace('<td class="RCelement">','')
+		  line=re.sub(r"</td>$",'',line)
+		  print line
+
 	
 	fd2.close()
 getdognames()
